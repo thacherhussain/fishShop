6611a1be2f957953b7a9c44f86795daf05fa3bd2
THACHER <thachert1d@gmail.com>
2016-11-04 12:38:43 -0700

    running with nested views

--- app/app.js
+++ app/app.js
@@ -2,12 +2,15 @@ import angular from 'angular';
 import angularMaterialize from 'angular-materialize';
 import uiRouter from 'angular-ui-router';
 
+
 angular.module('my-app', [angularMaterialize, uiRouter])
   .config(['$stateProvider', ($stateProvider) => {
     $stateProvider
       .state('home', {
         url: '',
-        templateUrl: 'views/home.html'
+        templateUrl: 'views/home.html',
+        controller: 'HomeCtrl',
+        controllerAs: 'homeCtrl'
       })
       .state('main', {
         url: '/',
