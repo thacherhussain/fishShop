a872f13ab2e19b04a0cc2bf76e6fc2d70f24638e
THACHER <thachert1d@gmail.com>
2016-11-06 18:50:07 -0800

    edits to app.js

--- app/app.js
+++ app/app.js
@@ -2,19 +2,16 @@ import angular from 'angular';
 import angularMaterialize from 'angular-materialize';
 import uiRouter from 'angular-ui-router';
 
-
 angular.module('my-app', [angularMaterialize, uiRouter])
   .config(['$stateProvider', ($stateProvider) => {
     $stateProvider
       .state('home', {
         url: '',
         templateUrl: 'views/home.html',
-        controller: 'HomeCtrl',
-        controllerAs: 'homeCtrl'
       })
       .state('main', {
         url: '/',
-        templateUrl: 'views/home.html'
+        templateUrl: 'views/home.html',
       })
       .state('diagonalley', {
         url: '/diagonalley',
