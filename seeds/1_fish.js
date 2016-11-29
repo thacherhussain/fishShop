e2f7753b82defe45ab5b9436e05131ceceb40dae
THACHER <thachert1d@gmail.com>
2016-11-28 19:53:59 -0800

    basic view updates and added seed fish

--- seeds/1_fish.js
+++ seeds/1_fish.js
@@ -24,11 +24,60 @@ exports.seed = function(knex) {
       {
         id: 3,
         name: 'Bass',
-        image: 'https://s-media-cache-ak0.pinimg.com/originals/c2/fd/66/c2fd663ea5e275f14109476a700e86e8.jpg',
+        image: 'http://cdn0.wideopenspaces.com/wp-content/uploads/2015/06/texas-bass-feature.jpg',
         rating: 3,
         price: 3796.95,
         on_sale: true
-      }]);
+      },
+      {
+        id: 4,
+        name: 'Cod and Chips',
+        image: 'http://citispotter.com/wp-content/uploads/2016/11/17-1.jpg',
+        rating: 5,
+        price: 8.99,
+        on_sale: false
+      },
+      {
+        id: 5,
+        name: "Po' boy sandwich",
+        image: 'http://1.bp.blogspot.com/-FdRSqaUhRA4/T4geHTphy4I/AAAAAAAAHAY/8E_K0WI21Kc/s1600/Shrimp%2BPo-Boy.jpg',
+        rating: 3,
+        price: 4.99,
+        on_sale: true
+      },
+      {
+        id: 6,
+        name: 'Catfish',
+        image: 'https://s-media-cache-ak0.pinimg.com/originals/10/b6/f2/10b6f2b88b5f0ba0cc67000aca8a16f8.jpg',
+        rating: 2,
+        price: 1299.01,
+        on_sale: false
+      },
+      {
+        id: 7,
+        name: 'Blowfish',
+        image: 'http://jpninfo.com/wp-content/uploads/2015/04/puffer-fish-3.jpg',
+        rating: 3,
+        price: 3019.95,
+        on_sale: true
+      },
+      {
+        id: 8,
+        name: 'Jaws',
+        image: 'http://www.rd.com/wp-content/uploads/2015/06/Jaws-anniversary-shark-pa.jpg',
+        rating: 3,
+        price: 1005.93,
+        on_sale: false
+      },
+      {
+        id: 9,
+        name: 'Nemo',
+        image: 'http://4.bp.blogspot.com/-CMSsYe-h_Vg/VjWDzd4W91I/AAAAAAAAA_c/NqEOTwreKPI/s1600/Animasi%2BNemo.jpeg',
+        rating: 3,
+        price: 124.95,
+        on_sale: false
+      },
+    ]);
     })
     .then(() => {
       return knex.raw(
