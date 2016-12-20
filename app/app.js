6d16ed846a1c3ea87407d1be082dfb93345588ad
THACHER <thachert1d@gmail.com>
2016-12-20 14:03:52 -0800

    no roboto!!!!

--- app/app.js
+++ app/app.js
@@ -2,24 +2,14 @@ import angular from 'angular';
 import angularMaterialize from 'angular-materialize';
 import uiRouter from 'angular-ui-router';
 
-import AuthCtrl from './auth/auth.controller';
-import AuthService from './auth/auth.service';
-
 import CartCtrl from './cart/cart.controller';
 import CartService from './cart/cart.service';
 
 import CatalogCtrl from './catalog/catalog.controller';
 import CatalogService from './catalog/catalog.service';
 
-import OrdersCtrl from './orders/orders.controller';
-import OrdersService from './orders/orders.service';
-
-import SignUpCtrl from './signup/signup.controller';
-import SignUpService from './signup/signup.service';
 
 angular.module('my-app', [angularMaterialize, uiRouter])
-  .service('AuthService', AuthService)
-  .controller('AuthCtrl', AuthCtrl)
 
   .controller('CartCtrl', CartCtrl)
   .service('CartService', CartService)
@@ -27,23 +17,20 @@ angular.module('my-app', [angularMaterialize, uiRouter])
   .service('CatalogService', CatalogService)
   .controller('CatalogCtrl', CatalogCtrl)
 
-  .controller('OrdersCtrl', OrdersCtrl)
-  .service('OrdersService', OrdersService)
-
-  .service('SignUpService', SignUpService)
-  .controller('SignUpCtrl', SignUpCtrl)
 
   .config(['$stateProvider', ($stateProvider) => {
     $stateProvider
       .state('home', {
         url: '',
-        templateUrl: 'views/home.html',
+        templateUrl: 'views/catalog.html',
         controller: 'CatalogCtrl',
         controllerAs: 'CatalogCtrl'
       })
       .state('main', {
         url: '/',
-        templateUrl: 'views/home.html'
+        templateUrl: 'views/catalog.html',
+        controller: 'CatalogCtrl',
+        controllerAs: 'CatalogCtrl'
       })
       .state('cart', {
         url: '/cart',
@@ -57,46 +44,15 @@ angular.module('my-app', [angularMaterialize, uiRouter])
         controller: 'CatalogCtrl',
         controllerAs: 'CatalogCtrl'
       })
-
-      // .state('user', {
-      //   url: '/user',
-      //   templateUrl: 'views/user/user.html'
-      // })
-      // .state('user.profile', {
-      //   url: '/profile',
-      //   templateUrl: 'views/user/profile.html'
-      // })
-      // .state('user.account', {
-      //   url: '/account',
-      //   templateUrl: 'views/user/account.html'
-      // })
-      .state('orders', {
-        url: '/orders',
-        templateUrl: 'views/orders.html',
-        controller: 'OrdersCtrl',
-        controllerAs: 'OrdersCtrl'
-      })
       .state('checkout', {
         url: '/checkout',
         templateUrl: 'views/checkout.html',
-        controller: 'OrdersCtrl',
-        controllerAs: 'OrdersCtrl'
+        controller: 'CatalogCtrl',
+        controllerAs: 'CatalogCtrl'
       })
       .state('thanks', {
         url: '/thanks',
         templateUrl: 'views/thanks.html'
-      })
-      .state('signIn', {
-        url: '/signIn',
-        templateUrl: 'views/signIn.html',
-        controller: 'AuthCtrl',
-        controllerAs: 'AuthCtrl'
-      })
-      .state('signUp', {
-        url: '/signUp',
-        templateUrl: 'views/signUp.html',
-        controller: 'AuthCtrl',
-        controllerAs: 'AuthCtrl'
       })
       ;
   }]);
